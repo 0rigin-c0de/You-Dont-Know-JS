cffb1aef304762adde16cfd301ecd0da71a8ffd9
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-14 10:33:05 +0530

    starting javascript

--- .src/IntoProgramming/Intro/first.js
+++ .src/IntoProgramming/Intro/first.js
@@ -2,4 +2,9 @@ a = 20;
 b = a * 2;
 console.log(b);
 
+// it shows the value of b
 alert(b);
+
+// it shows a popup notification ask about your age
+age = prompt("Please tell us your age");
+console.log(age);
