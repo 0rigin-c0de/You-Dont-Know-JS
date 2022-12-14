29a0a14ab28efca993dc6d68bb35b08bf78ad058
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-14 10:56:10 +0530

    learn values and types

---
+++ .src/IntoProgramming/Value_Types/first.js
@@ -0,0 +1,18 @@
+// string
+"I am a String ";
+"I am also a String";
+
+// number
+42;
+
+// boolean
+true;
+false;
+
+// convert number into string
+
+var a = "42";
+var b = Number(a);
+
+console.log(a);
+console.log(b);
