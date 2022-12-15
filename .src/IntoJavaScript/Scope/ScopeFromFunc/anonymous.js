75e4c1536d9199564e3ecd29b0990b5cbc6d77fd
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-15 22:27:52 +0530

    initialize anonymous function

---
+++ .src/IntoJavaScript/Scope/ScopeFromFunc/anonymous.js
@@ -0,0 +1,6 @@
+setTimeout(function () {
+  console.log("I waited 1 second!");
+}, 1000);
+
+/* this is called anonymous function because function has no name identifier but 
+function declarations cannot omit the name -- that would be illegal JS grammar.*/
