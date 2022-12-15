ecbc6e3cbd773ef4eecad19d7c2ee3130b8adf13
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-15 09:54:18 +0530

    initialize nested scope in javascript

---
+++ .src/IntoJavaScript/Scope/nestedScope.js
@@ -0,0 +1,15 @@
+function Intro() {
+  if (true) {
+    const name = "Hey i'm Shunnu";
+    console.log(name);
+    if (true) {
+      const say = "And I'm a frontend developer";
+      console.log(say);
+      if (true) {
+        const bio = "I'm currently learning Javascript";
+        console.log(bio);
+      }
+    }
+  }
+}
+Intro();
