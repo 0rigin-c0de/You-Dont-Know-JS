e54142020a0d87ceda87d87330b60a4ba1f9122a
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-15 22:19:02 +0530

    initial-commit

---
+++ .src/IntoJavaScript/Scope/ScopeFromFunc/namespaces.js
@@ -0,0 +1,17 @@
+var Library = {
+  awesome: "stuff",
+
+  doSomething() {
+    var a = 10;
+    if (true) {
+      console.log(a);
+    }
+  },
+  doSomethingElse() {
+    var b = 12;
+    if (true) {
+      console.log(b);
+    }
+  },
+};
+console.log(Library);
