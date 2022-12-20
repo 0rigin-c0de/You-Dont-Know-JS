b149dcb08375c40ff72b55bd4753bbaca3d274b6
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-20 14:06:58 +0530

    initial-commit

---
+++ .src/typesAndGrammar/Values/illustrate.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = a;
+
+b++;
+
+console.log(a, b);
+
+var c = [1, 2, 3];
+var d = c;
+d.push(4);
+
+console.log(c, d);
+
---
+++ .src/typesAndGrammar/Values/reference.js
@@ -0,0 +1,9 @@
+let name = "Marina";
+let name2 = name;
+console.log({ name, name2 });
+name = "shunnu";
+console.log({ name, name2 });
+
+const animals = ["Cat", "Dog", "Horse", "Snake"];
+let animals2 = animals;
+console.log({ animals, animals2 });
