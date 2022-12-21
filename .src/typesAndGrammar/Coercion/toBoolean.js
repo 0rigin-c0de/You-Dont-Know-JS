97dada506c5f476184ddec80bf50fd0cfa072df5
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-21 15:44:44 +0530

    initialize boolean coercion

--- .src/typesAndGrammar/Coercion/toBoolean.js
+++ .src/typesAndGrammar/Coercion/toBoolean.js
@@ -5,3 +5,20 @@ var c = new String("");
 var d = Boolean(a && b && c);
 
 console.log(d);
+
+var a = "0";
+var b = [];
+var c = {};
+
+var d = "";
+var e = 0;
+var f = null;
+var g;
+
+console.log(Boolean(a));
+console.log(Boolean(b));
+console.log(Boolean(c));
+console.log(Boolean(d));
+console.log(Boolean(e));
+console.log(Boolean(f));
+console.log(Boolean(g));
