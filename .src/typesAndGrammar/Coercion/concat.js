d20c5e2321690fce43faa8c0bf7c361a007e8b48
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-21 16:25:54 +0530

    initial-commit

--- .src/typesAndGrammar/Coercion/concat.js
+++ .src/typesAndGrammar/Coercion/concat.js
@@ -8,3 +8,11 @@ var d = 0;
 // it is just concatenation
 console.log(a + b);
 console.log(c + d);
+
+var num = [1, 2];
+var num1 = [3, 4];
+
+console.log(num + num1);
+
+/*Neither of these operands is a string, but clearly they were both
+ coerced to strings and then the string concatenation kicked in */
