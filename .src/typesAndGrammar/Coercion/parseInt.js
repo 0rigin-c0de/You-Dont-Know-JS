b16f027dd480aa382c98423b123492ec6aa635fb
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-21 15:39:08 +0530

    initialize parseInt

--- .src/typesAndGrammar/Coercion/parseInt.js
+++ .src/typesAndGrammar/Coercion/parseInt.js
@@ -3,4 +3,11 @@ var minute = parseInt(10);
 
 console.log("The time you selected was: " + hour + ":" + minute);
 
+var a = {
+  num: 21,
+  toString: function () {
+    return String(this.num * 2);
+  },
+};
 
+console.log(parseInt(a)); // 42
