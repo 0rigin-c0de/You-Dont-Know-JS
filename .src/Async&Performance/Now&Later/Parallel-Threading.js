ee811368035a8b5c3c7e5b0e990f47df56de9ab0
Shunnu <bhardwaj003sunil@gmail.com>
2022-12-23 15:11:39 +0530

    initial-commit

---
+++ .src/Async&Performance/Now&Later/Parallel-Threading.js
@@ -0,0 +1,17 @@
+var a = 20;
+function foo() {
+  a = a + 1;
+}
+function bar() {
+  a = a * 2;
+}
+
+ajax(
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json",
+  foo
+);
+
+ajax(
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json",
+  bar
+);
